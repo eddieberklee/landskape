edb45a9b72e1beef5a6d12b87630b355d18743ea
James <jamesmuerle@gmail.com>
2012-11-02 15:36:46 -0700

    Changed to be heroku compatible.

---
+++ web.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'bundler/setup'
+require 'sass'
+require 'sinatra/partial'
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+get '/' do
+  output = ""
+  output << partial( :"index-top" )
+  output << partial( :"navbar" )
+  output << partial( :"filters" )
+  output << partial( :"index" )
+  output << partial( :"index-bottom" )
+  output
+end
+
+get '/css/*.hi' do
+  set :views, File.dirname(__FILE__) + '/public/css'
+  sass "style".to_sym
+end
+
+get '/stylesheets/*.sass' do
+  set :views, File.dirname(__FILE__) + '/public/css'
+  filename = params[:splat].first
+  sass filename.to_sym
+end
