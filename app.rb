b123348623295039b6d1f96bc456d5c7bde997cc
Eddie <eddieberklee@gmail.com>
2012-11-04 18:31:46 -0800

    trying to fix for others

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 get '/' do
   set :views, File.dirname(__FILE__) + '/views'
   output = ""
-  output << partial( :"index-top", :locals => {title: 'Index'} )
-  output << partial( :"navbar", :locals => {username: 'amadeus'} )
+  output << partial( :"index-top", :locals => {:title => 'Index'} )
+  output << partial( :"navbar", :locals => {:username => 'amadeus'} )
   output << partial( :"filters" )
   output << partial( :"index" )
   output << partial( :"pics" )
@@ -21,8 +21,8 @@ end
 get '/upload' do
   set :views, File.dirname(__FILE__) + '/views'
   output = ""
-  output << partial( :"index-top", :locals => {title: 'Upload'} )
-  output << partial( :"navbar", :locals => {username: 'amadeus'} )
+  output << partial( :"index-top", :locals => {:title => 'Upload'} )
+  output << partial( :"navbar", :locals => {:username => 'amadeus'} )
   output << partial( :"filters" )
 
   output << partial( :"upload" )
@@ -38,12 +38,12 @@ end
 get '/user' do
   set :views, File.dirname(__FILE__) + '/views'
   output = ""
-  output << partial(:"index-top", :locals => {title: 'Upload'} )
-  output << partial( :"navbar", :locals => {username: 'amadeus'} )
-  output << partial(:"filters")
-  output << partial(:"userpage", :locals => {user: 'mozart', desc: 'I am a music god and prodigy. All at the same time'})
-  output << partial(:"pics")
-  output << partial(:"index-bottom")
+  output << partial( :"index-top", :locals => {:title => 'Upload'} )
+  output << partial( :"navbar", :locals => {:username => 'amadeus'} )
+  output << partial( :"filters")
+  output << partial( :"userpage", :locals => {:user => 'mozart', :desc => 'I am a music god and prodigy. All at the same time'})
+  output << partial( :"pics")
+  output << partial( :"index-bottom")
   output
 end
 
