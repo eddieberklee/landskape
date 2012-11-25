6a8a9afe43c6e97720f1ccbed782fc069321aec3
James <jamesmuerle@gmail.com>
2012-11-24 18:30:23 -0800

    Trying to get the DB setup

--- public/db.rb
+++ public/db.rb
@@ -3,20 +3,74 @@ require 'uri'
 
 puts 'I am a db and should be run'
 
-#db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/ddqa61sv0v5pnm')
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-database = 'ddqa61sv0v5pnm';
-host = 'ec2-23-21-161-255.compute-1.amazonaws.com';
-port = 5432;
-user = 'xpvzwbwbbksqvx';
-password = 'Wt5HmBrhT0XqpQoBF2RUBODKdr';
+puts db;
+#database = 'ddqa61sv0v5pnm';
+#host = 'ec2-23-21-161-255.compute-1.amazonaws.com';
+#port = 5432;
+#user = 'xpvzwbwbbksqvx';
+#password = 'Wt5HmBrhT0XqpQoBF2RUBODKdr';
 
 ActiveRecord::Base.establish_connection(
-    :adapter  => 'postgresql',
-    :host     => host,
-    :port     => port,
-    :username => user,
-    :password => password,
-    :database => database,
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
     :encoding => 'utf8'
 )
+
+ActiveRecord::Schema.define do
+  create_table :albums do |table|
+    table.column :title, :string
+    table.column :performer, :string
+  end
+
+  create_table :tracks do |table|
+    table.column :album_id, :integer
+    table.column :track_number, :integer
+    table.column :title, :string
+  end
+end
+
+class Album < ActiveRecord::Base
+  has_many :tracks
+end
+
+class Track < ActiveRecord::Base
+  belongs_to :album
+end
+
+album = Album.create(:title => 'Black and Blue',
+                     :performer => 'The Rolling Stones')
+album.tracks.create(:track_number => 1, :title => 'Hot Stuff')
+album.tracks.create(:track_number => 2, :title => 'Hand Of Fate')
+album.tracks.create(:track_number => 3, :title => 'Cherry Oh Baby ')
+album.tracks.create(:track_number => 4, :title => 'Memory Motel ')
+album.tracks.create(:track_number => 5, :title => 'Hey Negrita')
+album.tracks.create(:track_number => 6, :title => 'Fool To Cry')
+album.tracks.create(:track_number => 7, :title => 'Crazy Mama')
+album.tracks.create(:track_number => 8,
+                    :title => 'Melody (Inspiration By Billy Preston)')
+
+album = Album.create(:title => 'Sticky Fingers',
+                     :performer => 'The Rolling Stones')
+album.tracks.create(:track_number => 1, :title => 'Brown Sugar')
+album.tracks.create(:track_number => 2, :title => 'Sway')
+album.tracks.create(:track_number => 3, :title => 'Wild Horses')
+album.tracks.create(:track_number => 4,
+                    :title => 'Can\'t You Hear Me Knocking')
+album.tracks.create(:track_number => 5, :title => 'You Gotta Move')
+album.tracks.create(:track_number => 6, :title => 'Bitch')
+album.tracks.create(:track_number => 7, :title => 'I Got The Blues')
+album.tracks.create(:track_number => 8, :title => 'Sister Morphine')
+album.tracks.create(:track_number => 9, :title => 'Dead Flowers')
+album.tracks.create(:track_number => 10, :title => 'Moonlight Mile')
+
+puts Album.find(1).tracks.length
+puts Album.find(2).tracks.length
+
+puts Album.find_by_title('Sticky Fingers').title
+puts Track.find_by_title('Fool To Cry').album_id
