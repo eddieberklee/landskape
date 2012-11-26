60f61da722f7fd58d023f87acb4c01f28a7e0d01
James <jamesmuerle@gmail.com>
2012-11-25 21:07:40 -0800

    Getting the database to look right

--- public/db.rb
+++ public/db.rb
@@ -26,54 +26,92 @@ ActiveRecord::Base.establish_connection(
 )
 
 ActiveRecord::Schema.define do
-  create_table :albums do |table|
-    table.column :title, :string
-    table.column :performer, :string
+  if (!table_exists?(:users))
+    create_table :users do |table|
+      table.column :username, :string
+      table.column :password, :string
+    end
   end
 
-  create_table :tracks do |table|
-    table.column :album_id, :integer
-    table.column :track_number, :integer
-    table.column :title, :string
+  if (!table_exists?(:photos))
+    create_table :photos do |table|
+      table.column :title, :string
+      table.column :description, :string
+      table.column :city, :string
+      table.column :country, :string
+      table.column :state, :string
+      table.column :file_location, :string
+    end
+  end
+
+  if (!table_exists?(:comments))
+    create_table :comments do |table|
+      table.column :comment, :string
+    end
+  end
+
+  if (!table_exists?(:likes))
+    create_table :likes do |table|
+      table.column :comment, :string
+    end
   end
 end
 
-class Album < ActiveRecord::Base
-  has_many :tracks
+class User < ActiveRecord::Base
+  has_many :photos
+  has_many :comments
+  has_many :likes
 end
 
-class Track < ActiveRecord::Base
-  belongs_to :album
+class Photo < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments
+  has_many :likes
 end
 
-album = Album.create(:title => 'Black and Blue',
-                     :performer => 'The Rolling Stones')
-album.tracks.create(:track_number => 1, :title => 'Hot Stuff')
-album.tracks.create(:track_number => 2, :title => 'Hand Of Fate')
-album.tracks.create(:track_number => 3, :title => 'Cherry Oh Baby ')
-album.tracks.create(:track_number => 4, :title => 'Memory Motel ')
-album.tracks.create(:track_number => 5, :title => 'Hey Negrita')
-album.tracks.create(:track_number => 6, :title => 'Fool To Cry')
-album.tracks.create(:track_number => 7, :title => 'Crazy Mama')
-album.tracks.create(:track_number => 8,
-                    :title => 'Melody (Inspiration By Billy Preston)')
+class Comment < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :photo
+end
 
-album = Album.create(:title => 'Sticky Fingers',
-                     :performer => 'The Rolling Stones')
-album.tracks.create(:track_number => 1, :title => 'Brown Sugar')
-album.tracks.create(:track_number => 2, :title => 'Sway')
-album.tracks.create(:track_number => 3, :title => 'Wild Horses')
-album.tracks.create(:track_number => 4,
-                    :title => 'Can\'t You Hear Me Knocking')
-album.tracks.create(:track_number => 5, :title => 'You Gotta Move')
-album.tracks.create(:track_number => 6, :title => 'Bitch')
-album.tracks.create(:track_number => 7, :title => 'I Got The Blues')
-album.tracks.create(:track_number => 8, :title => 'Sister Morphine')
-album.tracks.create(:track_number => 9, :title => 'Dead Flowers')
-album.tracks.create(:track_number => 10, :title => 'Moonlight Mile')
+class Like < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :photo
+end
 
-puts Album.find(1).tracks.length
-puts Album.find(2).tracks.length
+user = User.create(:username => 'James', :password => 'gobears')
+photo = user.photos.create(:title => 'El Capitan', :description => 'So beautiful etc.',
+                           :city => 'Yosemite', :country => 'USA', :state => 'CA',
+                           :file_location => '/pics/elcapitan.png')
 
-puts Album.find_by_title('Sticky Fingers').title
-puts Track.find_by_title('Fool To Cry').album_id
+#album = Album.create(:title => 'Black and Blue',
+#                     :performer => 'The Rolling Stones')
+#album.tracks.create(:track_number => 1, :title => 'Hot Stuff')
+#album.tracks.create(:track_number => 2, :title => 'Hand Of Fate')
+#album.tracks.create(:track_number => 3, :title => 'Cherry Oh Baby ')
+#album.tracks.create(:track_number => 4, :title => 'Memory Motel ')
+#album.tracks.create(:track_number => 5, :title => 'Hey Negrita')
+#album.tracks.create(:track_number => 6, :title => 'Fool To Cry')
+#album.tracks.create(:track_number => 7, :title => 'Crazy Mama')
+#album.tracks.create(:track_number => 8,
+#                    :title => 'Melody (Inspiration By Billy Preston)')
+#
+#album = Album.create(:title => 'Sticky Fingers',
+#                     :performer => 'The Rolling Stones')
+#album.tracks.create(:track_number => 1, :title => 'Brown Sugar')
+#album.tracks.create(:track_number => 2, :title => 'Sway')
+#album.tracks.create(:track_number => 3, :title => 'Wild Horses')
+#album.tracks.create(:track_number => 4,
+#                    :title => 'Can\'t You Hear Me Knocking')
+#album.tracks.create(:track_number => 5, :title => 'You Gotta Move')
+#album.tracks.create(:track_number => 6, :title => 'Bitch')
+#album.tracks.create(:track_number => 7, :title => 'I Got The Blues')
+#album.tracks.create(:track_number => 8, :title => 'Sister Morphine')
+#album.tracks.create(:track_number => 9, :title => 'Dead Flowers')
+#album.tracks.create(:track_number => 10, :title => 'Moonlight Mile')
+#
+#puts Album.find(1).tracks.length
+#puts Album.find(2).tracks.length
+#
+#puts Album.find_by_title('Sticky Fingers').title
+#puts Track.find_by_title('Fool To Cry').album_id
